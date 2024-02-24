ad1c4b51a0b9b8e42fbce3e373e3774a97d51c64
Kevin Hu <kevinhu229@gmail.com>
2024-02-24 01:08:34 -0800

    add animations

--- src/lib/components/Intro.svelte
+++ src/lib/components/Intro.svelte
@@ -1,215 +1,209 @@
 <script>
     import { onMount } from 'svelte';
-
     let animationDone = false;
-    let prevName = "";
-    let velocity = 40;
-    let interval;
-    let introPlayed = false;
-    var hintIndex = 0;
-    var skip = false;
-    const re = new RegExp("^[a-z0-9]+$", "i");
-    const hints = [
-    "Enter Your Name",
-    "<enter> To Cont."
-    ]   
-    const fonts = [
-    "'Open Sans', sans-serif",
-    "'Wingdings', sans-serif", // Note: Wingdings may not display text as expected in HTML documents
-    "'Arial', sans-serif",
-    "'Black And White Picture', system-ui",
-    "'DotGothic16', sans-serif"
-    ];
-    let noTextSpan = document.getElementById("no-text");
-    const hintInterval = setInterval(updateHint, 5030);
-    const inputCursor = document.getElementsByClassName('input-cursor')[0];
-    const arrow = document.getElementById("arrow");
 
-    function updateHint() {
-        noTextSpan.textContent = hints[++hintIndex % hints.length];
-    }
+    onMount(() => {
+        let velocity = 40;
+        let interval;
+        let introPlayed = false;
+        var hintIndex = 0;
+        var skip = false;
+        const re = new RegExp("^[a-z0-9]+$", "i");
+        const hints = [
+        "Enter Your Name",
+        "<enter> To Cont."
+        ]
+        const fonts = [
+        "'Open Sans', sans-serif",
+        "'Wingdings', sans-serif", // Note: Wingdings may not display text as expected in HTML documents
+        "'Arial', sans-serif",
+        "'Black And White Picture', system-ui",
+        "'DotGothic16', sans-serif"
+        ];
 
-    function transition() {
-        if (window.introHandlerRef) {
-            if (!skip) {
-            document.removeEventListener('keydown', window.introHandlerRef);
-            document.removeEventListener('input', window.introHandlerRef);
-            delete window.introHandlerRef;
+        function updateHint() {
+            document.getElementById("no-text").textContent = hints[++hintIndex % hints.length];
+        }
+
+        const hintInterval = setInterval(updateHint, 5030);
+
+        function transition() {
+            if (window.introHandlerRef) {
+                if (!skip) {
+                document.removeEventListener('keydown', window.introHandlerRef);
+                document.removeEventListener('input', window.introHandlerRef);
+                delete window.introHandlerRef;
+                }
+            }
+            clearInterval(hintInterval);
+            const inputCursor = document.getElementsByClassName('input-cursor')[0];
+            const arrow = document.getElementById("arrow");
+            arrow.style.opacity = "0";
+            inputCursor.remove();
+            
+            const textSpan = document.getElementById("text");
+            textSpan.setAttribute("title", textSpan.textContent);
+            textSpan.style.position = "fixed";
+            textSpan.style.top = "50%";
+            textSpan.style.left = "50%";
+            textSpan.style.transform = "translate(-50%, -50%)";
+            
+            const greySpan = textSpan.cloneNode(true);
+            const redSpan = textSpan.cloneNode(true);
+            textSpan.style.animation = "glitch 1s linear infinite";
+            
+            redSpan.style.color = "#FF0000";
+            redSpan.style.animation = "glitch-red 1s linear infinite";
+            redSpan.style.zIndex = "3";
+            greySpan.style.color = "#ACA9A5";
+            greySpan.style.zIndex = "3";
+            greySpan.style.animation = "glitch-grey 1s linear infinite";
+            
+            textSpan.parentNode.appendChild(greySpan);
+            textSpan.parentNode.appendChild(redSpan);
+            
+            var fontIndex = 0; 
+            interval = setInterval(() => {
+                textSpan.style.fontFamily = fonts[fontIndex % fonts.length];
+                redSpan.style.fontFamily = fonts[fontIndex % fonts.length];
+                greySpan.style.fontFamily = fonts[fontIndex % fonts.length];
+                if (++fontIndex > fonts.length * 2) {
+                const logoSpan = document.getElementById("logo-container");
+                const progressSpan = document.getElementById("progress-bar");
+                const introWrapperSpan = document.getElementById("intro-wrapper");
+                logoSpan.style.display = 'flex';
+                progressSpan.remove();
+                introWrapperSpan.innerHTML = '';
+                introWrapperSpan.remove();
+                textSpan.style.animation = "initial";
+                textSpan.style.fontFamily = "'DotGothic16', sans-serif";
+                redSpan.remove();
+                greySpan.remove();
+                logoSpan.innerHTML = "<img id='logo' src='https://media.discordapp.net/attachments/1156704793658671166/1209668504043790377/free_will_logo_glitch_red.png?ex=65e7c295&is=65d54d95&hm=f807277c3db0cc11a6e0c58af27116fbf1593d4f89a3384626b3fa7d52e46449&=&format=webp&quality=lossless&width=1394&height=271'> <span class='regular-text' id='subheader'>Available May 2024";
+                clearInterval(interval);
+                }
+            }, 100);
+        }
+
+        function getCharacterWidth(character, element) {
+            const tempElement = document.createElement('span');
+            tempElement.style.visibility = 'hidden';
+            tempElement.style.position = 'absolute';
+            tempElement.style.font = getComputedStyle(element).font; 
+            tempElement.textContent = character;
+            document.body.appendChild(tempElement);
+            
+            const width = tempElement.offsetWidth;
+            document.body.removeChild(tempElement);
+            
+            return width;
+        }
+        
+        document.addEventListener('DOMContentLoaded', () => {
+            const noTextElement = document.getElementById('no-text');
+            const inputCursor = document.querySelector('.input-cursor');
+
+            const sampleText = noTextElement.textContent || noTextElement.innerText;
+            const averageCharWidth = getCharacterWidth(sampleText, noTextElement) / sampleText.length;
+
+            inputCursor.style.width = `${averageCharWidth}px`;
+        });
+
+        window.addEventListener('resize', () => {
+            if (!introPlayed)
+            {
+                const noTextElement = document.getElementById('no-text');
+                const inputCursor = document.querySelector('.input-cursor');
+                const sampleText = noTextElement.textContent || noTextElement.innerText; 
+                const averageCharWidth = getCharacterWidth(sampleText, noTextElement) / sampleText.length;
+
+                inputCursor.style.width = `${averageCharWidth}px`;
+            
+                const textKeySpan = document.getElementById('text');
+                if (textKeySpan.textContent.length !== 0) {
+                const textWidth = textKeySpan.offsetWidth;
+                inputCursor.style.marginLeft = `${textWidth}px`;
+                } else {
+                inputCursor.style.marginLeft = "0px";
+                }
+            }
+        });
+
+        function updateProgressBar(x) {
+            if (x) {
+                const progressBar = document.getElementById("progress-bar");
+                const pbComputedStyle = getComputedStyle(progressBar);
+                const height = parseFloat(pbComputedStyle.getPropertyValue("--height")) || 0;
+                if (height == 100) {
+                clearInterval(interval);
+                transition();
+                introPlayed = true;
+                }
+                if (skip) { velocity *= 1.5; }
+                progressBar.style.setProperty('--height', Math.min(height + velocity/200, 100));
+            } else {
+                const progressBar = document.getElementById("progress-bar");
+                const pbComputedStyle = getComputedStyle(progressBar)
+                const height = parseFloat(pbComputedStyle.getPropertyValue("--height")) || 0;
+                progressBar.style.setProperty('--height', Math.max(0, height - 3 * velocity/200));
             }
         }
-        clearInterval(hintInterval);
-        arrow.style.opacity = "0";
-        inputCursor.remove();
-        
-        const textSpan = document.getElementById("text");
-        textSpan.setAttribute("title", textSpan.textContent);
-        textSpan.style.position = "fixed";
-        textSpan.style.top = "50%";
-        textSpan.style.left = "50%";
-        textSpan.style.transform = "translate(-50%, -50%)";
-        
-        const greySpan = textSpan.cloneNode(true);
-        const redSpan = textSpan.cloneNode(true);
-        textSpan.style.animation = "glitch 1s linear infinite";
-        
-        redSpan.style.color = "#FF0000";
-        redSpan.style.animation = "glitch-red 1s linear infinite";
-        redSpan.style.zIndex = "3";
-        greySpan.style.color = "#ACA9A5";
-        greySpan.style.zIndex = "3";
-        greySpan.style.animation = "glitch-grey 1s linear infinite";
-        
-        textSpan.parentNode.appendChild(greySpan);
-        textSpan.parentNode.appendChild(redSpan);
-        
-        var fontIndex = 0; 
-        interval = setInterval(() => {
-            textSpan.style.fontFamily = fonts[fontIndex % fonts.length];
-            redSpan.style.fontFamily = fonts[fontIndex % fonts.length];
-            greySpan.style.fontFamily = fonts[fontIndex % fonts.length];
-            if (++fontIndex > fonts.length * 2) {
-            const titleSpan = document.getElementById("logo-container");
-            const progressSpan = document.getElementById("progress-bar");
-            const introWrapperSpan = document.getElementById("intro-wrapper");
-            progressSpan.remove();
-            introWrapperSpan.innerHTML = '';
-            introWrapperSpan.remove();
-            textSpan.style.animation = "initial";
-            textSpan.style.fontFamily = "'DotGothic16', sans-serif";
-            redSpan.remove();
-            greySpan.remove();
-            titleSpan.innerHTML = "<img id='logo' src='https://media.discordapp.net/attachments/1156704793658671166/1209668504043790377/free_will_logo_glitch_red.png?ex=65e7c295&is=65d54d95&hm=f807277c3db0cc11a6e0c58af27116fbf1593d4f89a3384626b3fa7d52e46449&=&format=webp&quality=lossless&width=1394&height=271'> <span class='regular-text' id='subheader'>Available May 2024";
-            clearInterval(interval);
+
+        function intro(event, inputCursor, textSpan, noTextSpan, hiddenInput) {
+            if (event.key === "Enter") {
+                event.preventDefault();
+                skip = true;
+                document.removeEventListener('keydown', window.introHandlerRef);
+                document.removeEventListener('input', window.introHandlerRef);
+                delete window.introHandlerRef;
+                if (textSpan.textContent.length == 0) {
+                textSpan.textContent = "Player";
+                noTextSpan.style.display = "none";
+                } 
+                clearInterval(interval);
+                introPlayed = true;
+                interval = setInterval(updateProgressBar, 5, true);
+                return;
             }
-        }, 100);
-    }
-
-    function getCharacterWidth(character, element) {
-        const tempElement = document.createElement('span');
-        tempElement.style.visibility = 'hidden';
-        tempElement.style.position = 'absolute';
-        tempElement.style.font = getComputedStyle(element).font; 
-        tempElement.textContent = character;
-        document.body.appendChild(tempElement);
-        
-        const width = tempElement.offsetWidth;
-        document.body.removeChild(tempElement);
-        
-        return width;
-    }
-
-    indow.addEventListener('resize', () => {
-  if (!introPlayed)
-  {
-        const noTextElement = document.getElementById('no-text');
-        const inputCursor = document.querySelector('.input-cursor');
-        const sampleText = noTextElement.textContent || noTextElement.innerText; 
-        const averageCharWidth = getCharacterWidth(sampleText, noTextElement) / sampleText.length;
-
-        inputCursor.style.width = `${averageCharWidth}px`;
-    
-        const textKeySpan = document.getElementById('text');
-        if (textKeySpan.textContent.length !== 0) {
-        const textWidth = textKeySpan.offsetWidth;
-        inputCursor.style.marginLeft = `${textWidth}px`;
-        } else {
-        inputCursor.style.marginLeft = "0px";
-        }
-    }
-    });
-
-    document.addEventListener('DOMContentLoaded', () => {
-        const noTextElement = document.getElementById('no-text');
-        const inputCursor = document.querySelector('.input-cursor');
-
-        const sampleText = noTextElement.textContent || noTextElement.innerText;
-        const averageCharWidth = getCharacterWidth(sampleText, noTextElement) / sampleText.length;
-
-        inputCursor.style.width = `${averageCharWidth}px`;
-    });
-
-    function updateProgressBar(x) {
-        if (x) {
-            const progressBar = document.getElementById("progress-bar");
-            const pbComputedStyle = getComputedStyle(progressBar);
-            const height = parseFloat(pbComputedStyle.getPropertyValue("--height")) || 0;
-            if (height == 100) {
-            clearInterval(interval);
-            transition();
-            introPlayed = true;
-            }
-            if (skip) { velocity *= 1.5; }
-            progressBar.style.setProperty('--height', Math.min(height + velocity/200, 100));
-        } else {
-            const progressBar = document.getElementById("progress-bar");
-            const pbComputedStyle = getComputedStyle(progressBar)
-            const height = parseFloat(pbComputedStyle.getPropertyValue("--height")) || 0;
-            progressBar.style.setProperty('--height', Math.max(0, height - 3 * velocity/200));
-        }
-    }
-
-    function intro(event, inputCursor, textSpan, noTextSpan, hiddenInput) {
-        if (event.key === "Enter") {
-            event.preventDefault();
-            skip = true;
-            document.removeEventListener('keydown', window.introHandlerRef);
-            document.removeEventListener('input', window.introHandlerRef);
-            delete window.introHandlerRef;
-            if (textSpan.textContent.length == 0) {
-            textSpan.textContent = "Player";
-            noTextSpan.style.display = "none";
-            } 
-            clearInterval(interval);
-            introPlayed = true;
-            interval = setInterval(updateProgressBar, 5, true);
-            return;
-        }
-        switch(event.type) {
-            case 'input':
+            switch(event.type) {
+                case 'input':
                 if (re.test(event.target.value) && textSpan.textContent.length + event.target.value.length < 16) {
                     textSpan.textContent += event.target.value;
                     event.target.value = "";
                 }
-            break;
-            case 'keydown':
+                break;
+                case 'keydown':
                 if (event.key === "Backspace")  {
                     textSpan.textContent = textSpan.textContent.slice(0, -1);
                 }
-            break;
+                break;
+            }
+
+            if (textSpan.textContent.length !== 0) {
+                clearInterval(interval);
+                interval = setInterval(updateProgressBar, 5, true);
+                const textWidth = textSpan.offsetWidth;
+                inputCursor.style.marginLeft = `${textWidth}px`;
+            } else {
+                clearInterval(interval);
+                interval = setInterval(updateProgressBar, 5, false);
+                inputCursor.style.marginLeft = "0px";
+            }
+            noTextSpan.style.display = textSpan.textContent.length !== 0 ? "none" : "block";
         }
 
-        if (textSpan.textContent.length !== 0) {
-            clearInterval(interval);
-            interval = setInterval(updateProgressBar, 5, true);
-            const textWidth = textSpan.offsetWidth;
-            inputCursor.style.marginLeft = `${textWidth}px`;
-        } else {
-            clearInterval(interval);
-            interval = setInterval(updateProgressBar, 5, false);
-            inputCursor.style.marginLeft = "0px";
+        if (!introPlayed) {
+            const inputCursor = document.querySelector('.input-cursor');
+            const textSpan = document.getElementById('text');
+            const noTextSpan = document.getElementById('no-text');
+            const hiddenInput = document.getElementById('hidden-input');
+            //hiddenInput.focus();
+            const introHandler = (event) => intro(event, inputCursor, textSpan, noTextSpan, hiddenInput); 
+            document.addEventListener('keydown', introHandler);
+            hiddenInput.addEventListener('input', introHandler);
+            window.introHandlerRef = introHandler; 
         }
-        noTextSpan.style.display = textSpan.textContent.length !== 0 ? "none" : "block";
-    }
-
-    if (!introPlayed) {
-        const inputCursor = document.querySelector('.input-cursor');
-        const textSpan = document.getElementById('text');
-        const noTextSpan = document.getElementById('no-text');
-        const hiddenInput = document.getElementById('hidden-input');
-        //hiddenInput.focus();
-        const introHandler = (event) => intro(event, inputCursor, textSpan, noTextSpan, hiddenInput); 
-        document.addEventListener('keydown', introHandler);
-        hiddenInput.addEventListener('input', introHandler);
-        window.introHandlerRef = introHandler; 
-    }
-
-    onMount(() => {
-        // Adjust this to match the duration of your animation
-        setTimeout(() => {
-        animationDone = true;
-        // Emit an event or change a store value to notify the app the animation is done
-        }, 5000); // Assume 5000ms = 5 seconds for the animation
-    });
+    }, 5000); // Assume 5000ms = 5 seconds for the animation
 </script>
 
 <style>
@@ -324,7 +318,7 @@
     }
 
     #logo-container {
-        display: flex;
+        display: none;
         flex-direction: column;
         height: 100vh;
         justify-content: center;
@@ -413,8 +407,8 @@
 
 {#if !animationDone}
     <div id="intro-wrapper" class="wrapper">
-        <form id = "text-container" on:submit|preventDefault={handleSubmit}>
-        <span id="arrow">&#8594</span>
+        <form id = "text-container" on:submit|preventDefault>
+        <span id="arrow">></span>
         <div id="input-container">
             <input type="text" id="hidden-input" class="real-input" maxlength="16">
             <span class="input-cursor"></span>
