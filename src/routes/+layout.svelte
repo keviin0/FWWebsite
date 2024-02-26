e1877d0a52ef097dc0afa9fa328b0f717975ae7e
Kevin Hu <kevinhu229@gmail.com>
2024-02-25 16:02:46 -0800

    add analytics fr t his time

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import './styles.css';
-	import { inject } from '@vercel/analytics'
+	import { inject } from '@vercel/analytics';
+	import { dev } from '$app/environment';
 	import Intro from '../lib/components/Intro/Intro.svelte';
 	import { animationDone } from './stores';
     import About from '$lib/components/About.svelte';
@@ -11,6 +12,7 @@
 		animation = value;
 	});
 	
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 {#if !animation}
