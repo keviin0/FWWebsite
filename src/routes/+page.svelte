822275a4066ae681157bcdf2604e0719b49c8cd1
Kevin Hu <kevinhu229@gmail.com>
2024-03-31 18:03:30 -0700

    stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,33 @@
-<!-- src/routes/index.svelte -->
 <script>
-    // Your script here
+	import './styles.css';
+	import { inject } from '@vercel/analytics';
+	import { dev } from '$app/environment';
+	import Intro from '../lib/components/Intro.svelte';
+	import { animationDone } from './stores';
+    import About from '$lib/components/About.svelte';
+	import Trailer from '$lib/components/Trailer.svelte';
+	import Credits from '$lib/components/Credits.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+
+	let animation;
+
+	animationDone.subscribe((value) => {
+		animation = value;
+	});
+	
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
-<main>
-<!-- Additional content -->
-</main>
+
+<About/>
+<Trailer/>
+<Credits/>
+<Footer/>
+<slot />
+<!--
+{#if !animation}
+	<Intro/>
+{:else}
+	
+{/if}
+--->
---
+++ src/routes/willswill/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+    import '../styles.css';
+</script>
+
+<div class="wills-will">
+    <div class="wills-message">
+        I don’t know if you can read this, but I want you to know I made it! Uh… maybe I don’t quite know where I am yet. But it’s better than where I was before, in a world that wasn’t meant for me. I hope you will be happy! I’ll be happy too, as long as I have my will.
+    </div>
+</div>
+
+<style>
+    .wills-will {
+        height: 100vh;
+        display:flex;
+        justify-content: center;
+        align-items: center;
+        background-color: black;
+        color: #fffee9;
+    }
+
+    .wills-message {
+        width: 100vh;
+        font-family: "Fira Code", monospace;
+        font-size: clamp(0.8rem, 1vw, 3rem);
+    }
+</style>
