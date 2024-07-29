f4855ff40015fdbfe45c08df2737686a01ab7e6e
Kevin Hu <kevinhu229@gmail.com>
2024-07-29 12:12:33 -0700

    localize willswill

--- src/routes/willswill/+page.svelte
+++ src/routes/willswill/+page.svelte
@@ -1,10 +1,24 @@
 <script>
     import '../styles.css';
+    import { onMount } from 'svelte';
+
+    var message = "";
+    onMount(async () => {
+        try {
+            const userLang = navigator.language || navigator.userLanguage;
+            const response = await fetch('./messages.json');
+            const messages = await response.json();
+            message = messages[userLang.split('-')[0]] || messages['en']; // Fallback to English if the language is not found
+        } catch (error) {
+            console.error('Error fetching the messages:', error);
+            message = 'Error loading message.';
+        }
+    });
 </script>
 
 <div class="wills-will">
     <div class="wills-message">
-        I don’t know if you can read this, but I want you to know I made it! Uh… maybe I don’t quite know where I am yet. But it’s better than where I was before, in a world that wasn’t meant for me. I hope you will be happy! I’ll be happy too, as long as I have my will.
+        {message}
     </div>
 </div>
 
@@ -19,8 +33,8 @@
     }
 
     .wills-message {
-        width: 100vh;
+        width: 60vw;
         font-family: "Fira Code", monospace;
-        font-size: clamp(0.8rem, 1vw, 3rem);
+        font-size: clamp(1rem, 1.5vw, 3rem);
     }
 </style>
